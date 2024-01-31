359aaf4da8e80fd7a0f989d119d6e4a0767b7909
awatefaissaoui <awatef.aissaoui23@gmail.com>
2024-01-31 15:41:13 +0100

    cloudinary intergation

--- public/src/Uploader.jsx
+++ public/src/Uploader.jsx
@@ -1,16 +1,23 @@
 import React, { useState } from 'react';
+import axios from "axios"
 
 const Uploader = () => {
   const [fileContent, setFileContent] = useState('');
+  const [preview, setPreview] = useState('');
   
   const handleFileChange = (event) => {
+    console.log(event.target.value)
+    console.log(event)
+    setPreview(event.target.value)
     const file = event.target.files[0];
+    console.log(file)
     return new Promise((resolve, reject) => {
         const fileReader = new FileReader();
         fileReader.readAsDataURL(file);
   
         fileReader.onload = () => {
             setFileContent(fileReader.result)
+            setPreview(fileReader.result)
           resolve(fileReader.result);
         };
   
@@ -22,16 +29,26 @@ const Uploader = () => {
     
   };
 
+   const handleUpload  = async function (){
+    const res = await  axios.post("http://localhost:3000/upload", {image:fileContent})
+    console.log(res)
+
+   }
+
+
   return (
     <div>
       <input type="file" onChange={handleFileChange} />
       <div>
-        <strong>File Content:</strong>
-        <pre>{fileContent}</pre>
+        <img src={preview} alt="" />
+       
 
 
         
       </div>
+      <button
+       onClick={handleUpload}
+      >Upload</button>
     </div>
   );
 };
--- server.js
+++ server.js
@@ -4,18 +4,22 @@ const app = express()
 const mongoose= require('mongoose')
 
 const cloudinary = require('cloudinary').v2
+const cors = require('cors')
 
+require('dotenv').config()
 
 cloudinary.config({
-    cloud_name: process.env.CLOUD_NAME,
+    cloud_name: process.env.CLOUD,
     api_key: process.env.CLOUD_KEY,
     api_secret: process.env.CLOUD_SECRET,
     secure: true,
   });
   
 
-
-require('dotenv').config()
+app.use(cors({
+  origin:"*",
+  credentials : true
+}))
 
 mongoose.connect(process.env.DB).then(() => {
     console.log("database connected ");
@@ -26,21 +30,40 @@ mongoose.connect(process.env.DB).then(() => {
     extended:true
   }))
 
+  app.use(express.json())
 
 
-app.post('/upload' , (req,res)=>{
+
+console.log(process.env.CLOUD)
+
+app.post('/upload' ,async (req,res)=>{
+  
 
 
-    try {
-        console.log(req)
+  
 
-        res.status(200).send('uploaded')
+       await  new Promise((resolve, reject) => {
+          cloudinary.uploader.upload(
+            req.body.image,
+            { folder: "images", resource_type: "auto" },
+            async function (error, result) {
+              console.log(error ,result)
+              if (error) {
+                console.log("Upload error:", error);
+                reject(error);
+                res.status(400).json({error : " not ok"})
+
+                return;
+              }
+              resolve(result);
+              res.status(200).json({data : result})
+
+            }
+            );
+          })
+          
         
-    } catch (error) {
-        res.status(500).send('error')
 
-        
-    }
 })
 
 
